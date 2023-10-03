9099dcf47409830394e885c9052d7dcb52f97c6c
NoemiVargas <vargasj.noemi@gmail.com>
2023-10-03 12:57:24 -0500

    feat: Added Event Model in Domain

---
+++ JazaniActividad.Domain/Admins/Models/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JazaniActividad.Domain.Admins.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public int CreatorEmployeeId { get; set; }
+        public int OwnerEmployeeId { get; set; }
+        public DateTimeOffset RegistrationDate { get; set; }
+        public bool State { get; set; }
+
+    }
+}
---
+++ JazaniActividad.Domain/Admins/Repositories/IEventRepository.cs
@@ -0,0 +1,16 @@
+﻿using JazaniActividad.Domain.Admins.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JazaniActividad.Domain.Admins.Repositories
+{
+    public interface IEventRepository
+    {
+        Task<IReadOnlyList<Event>> FindAllAsync();
+        Task<Event?> FindByIdAsync(int id);
+        Task<Currency?> SaveAsync(Currency currency);
+    }
+}
