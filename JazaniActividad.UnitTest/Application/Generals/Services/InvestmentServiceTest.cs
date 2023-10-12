53ef8cea4e2e30c125ab5212ea7dbe3eb445c603
NoemiVargas <vargasj.noemi@gmail.com>
2023-10-12 15:59:50 -0500

    feat: Added Invesment complete test

--- JazaniActividad.UnitTest/Application/Generals/Services/InvestmentServiceTest.cs
+++ JazaniActividad.UnitTest/Application/Generals/Services/InvestmentServiceTest.cs
@@ -1,13 +1,13 @@
-﻿using Moq;
-using AutoFixture;
+﻿using AutoFixture;
 using AutoMapper;
+using JazaniActividad.Application.Generals.Dtos.Invesments;
+using JazaniActividad.Application.Generals.Dtos.Invesments.Profiles;
 using JazaniActividad.Application.Generals.Services;
 using JazaniActividad.Application.Generals.Services.Implementations;
 using JazaniActividad.Domain.Generals.Models;
 using JazaniActividad.Domain.Generals.Repositories;
+using Moq;
 using Xunit;
-using Castle.Core.Logging;
-using JazaniActividad.Application.Generals.Dtos.Invesments;
 
 namespace JazaniActividad.UnitTest.Application.Generals.Services
 {
@@ -15,47 +15,271 @@ namespace JazaniActividad.UnitTest.Application.Generals.Services
     {
 
 
-        /*
-        Task<IReadOnlyList<InvesmentDto>> FindAllAsync();
-        Task<InvesmentDto?> FindByIdAsync(int id);
-        Task<InvesmentDto> CreateAsync(InvesmentSaveDto saveDto);
-        Task<InvesmentDto> EditAsync(int id, InvesmentSaveDto saveDto);
-        Task<InvesmentDto> DisabledAsync(int id);
-        */
+        Mock<IInvesmentRepository> _mockInvestmentRepository = new Mock<IInvesmentRepository>();
+        Mock<Microsoft.Extensions.Logging.ILogger<InvesmentService>> _mockILogger;
+        IMapper _mapper;
+        Fixture _fixture;
+
+
+    
         public InvestmentServiceTest()
         {
+            MapperConfiguration mapperConfiguration = new MapperConfiguration(c =>
+            {
+                c.AddProfile<InvesmentProfile>();
+            });
+            _mapper = mapperConfiguration.CreateMapper();
+            _fixture =new Fixture();
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            _mockInvestmentRepository = new Mock<IInvesmentRepository>();
+            _mockILogger = new Mock<Microsoft.Extensions.Logging.ILogger<InvesmentService>>();
         }
 
         [Fact]
         public async void returnInvesmentDtoWhenFindByIdAsync()
         {
             //Arrange
-            Fixture fixture = new Fixture();
-            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
-            Invesment invesment = fixture.Create<Invesment>();
+            Invesment invesment =  _fixture.Create<Invesment>();
             
-            
-            Mock<IInvesmentRepository> mockInvestmentRepository = new Mock<IInvesmentRepository>();
-            mockInvestmentRepository
+            _mockInvestmentRepository
                 .Setup(r => r.FindByIdAsync(It.IsAny<int>()))
                 .ReturnsAsync(invesment);
 
-            MapperConfiguration mapperConfiguration = new MapperConfiguration(c =>
-            {
-                c.AddProfile<Invesment>();
-            });
-
-            IMapper mapper = mapperConfiguration.CreateMapper();
-            Mock<Microsoft.Extensions.Loggin.ILogger<Invesment>> mockILogger = new Mock<Microsoft.Extensions.Loggin.ILogger<Invesment>>();
-
             //act
-            IInvesmentService invesmentService = InvesmentService(mockInvestmentRepository.Object, mapper, mockILogger.Object);
+            IInvesmentService invesmentService = new InvesmentService(_mockInvestmentRepository.Object, _mapper);
 
-            InvesmentDto invesmentDto = await invesmentService.FindByIdAsync(invesment.Id); 
+            InvesmentDto invesmentDto = await invesmentService.FindByIdAsync(invesment.Id);
 
             //Assert
-            Assert.Equals(invesment.Year, invesment.Description);
+            Assert.Equal(invesment.Year, invesmentDto.Year);
+        }
 
+
+        [Fact]
+        public async void returnInvesmentDtoWhenFindAllAsync()
+        {
+            //Arrays
+            IReadOnlyList<Invesment> invesments = _fixture.CreateMany<Invesment>(5)
+                .ToList();
+
+            _mockInvestmentRepository
+                .Setup(r => r.FindAllAsync())
+                .ReturnsAsync(invesments);
+
+
+            //Act
+            IInvesmentService invesmentService = new InvesmentService(_mockInvestmentRepository.Object, _mapper);
+            IReadOnlyList<InvesmentDto> invesmentDtos = await invesmentService.FindAllAsync();
+
+            //Assert
+            Assert.Equal(invesments.Count, invesmentDtos.Count);
+        }
+
+
+        [Fact]
+        public async void returnInvesmentDtoWhenCreateAsync()
+        {
+  
+            //Arrange
+            Invesment invesment = new()
+            {
+                Id = 1,
+                Amount = 200,
+                Year = 2023,
+                Description = "description",
+                MiningConcessionId = 500,
+                Invesmenttypeid = 1,
+                CurrencyTypeId = 2,
+                PeriodTypeid = 0,
+                MeasureUnitId = 0,
+                RegistrationDate = DateTime.Now,
+                State = false,
+                MonthName = "--Seleccione--",
+                MonthId = 0,
+                AccreditationCode = "2020",
+                AccountantCode = "9090",
+                HolderId = 4,
+                DeclaredTypeId = 0,
+                DocumentId = 5050,
+                InvesmentConceptId = 2,
+                Module = false,
+                Frecuency = 1,
+                Isdac = 1,
+                Metrictons = "",
+                DeclarationDate = DateTime.Now
+            };
+            _mockInvestmentRepository
+                .Setup(r => r.SaveAsync(It.IsAny<Invesment>()))
+                .ReturnsAsync(invesment);
+
+            //Act
+            InvesmentSaveDto invesmentSaveDto = new()
+            {
+                //Id = 1,
+                Amount = 200,
+                Year = 2023,
+                Description = "description",
+                MiningConcessionId = 500,
+                Invesmenttypeid = 1,
+                CurrencyTypeId = 2,
+                PeriodTypeid = 0,
+                MeasureUnitId = 0,
+                // RegistrationDate = DateTime.Now,
+                //State = false,
+                MonthName = "--Seleccione--",
+                MonthId = 0,
+                AccreditationCode = "2020",
+                AccountantCode = "9090",
+                HolderId = 4,
+                DeclaredTypeId = 0,
+                DocumentId = 5050,
+                InvesmentConceptId = 2,
+                Module = false,
+                Frecuency = 1,
+                Isdac = 1,
+                Metrictons = "",
+                DeclarationDate = DateTime.Now
+            };
+            IInvesmentService invesmentService = new InvesmentService(_mockInvestmentRepository.Object, _mapper);
+            InvesmentDto invesmentDto = await invesmentService.CreateAsync(invesmentSaveDto);
+
+
+            //Assert
+            Assert.Equal(invesment.Description, invesmentDto.Description);
+        }
+
+
+        [Fact]
+        public async void returnInvesmentDtoWhenEditAsync()
+        {
+            // 
+            int id = 1;
+            //Arrange
+            Invesment invesment = new()
+            {
+                Id = 1,
+                Amount = 200,
+                Year = 2023,
+                Description = "description",
+                MiningConcessionId = 500,
+                Invesmenttypeid = 1,
+                CurrencyTypeId = 2,
+                PeriodTypeid = 0,
+                MeasureUnitId = 0,
+                RegistrationDate = DateTime.Now,
+                State = false,
+                MonthName = "--Seleccione--",
+                MonthId = 0,
+                AccreditationCode = "2020",
+                AccountantCode = "9090",
+                HolderId = 4,
+                DeclaredTypeId = 0,
+                DocumentId = 5050,
+                InvesmentConceptId = 2,
+                Module = false,
+                Frecuency = 1,
+                Isdac = 1,
+                Metrictons = "",
+                DeclarationDate = DateTime.Now
+            };
+
+            _mockInvestmentRepository
+             .Setup(r => r.FindByIdAsync(It.IsAny<int>()))
+             .ReturnsAsync(invesment);
+
+            _mockInvestmentRepository
+                .Setup(r => r.SaveAsync(It.IsAny<Invesment>()))
+                .ReturnsAsync(invesment);
+
+         
+
+
+            //Act
+            InvesmentSaveDto invesmentSaveDto = new()
+            {
+                //Id = 1,
+                Amount = 200,
+                Year = 2023,
+                Description = "description",
+                MiningConcessionId = 500,
+                Invesmenttypeid = 1,
+                CurrencyTypeId = 2,
+                PeriodTypeid = 0,
+                MeasureUnitId = 0,
+                // RegistrationDate = DateTime.Now,
+                //State = false,
+                MonthName = "--Seleccione--",
+                MonthId = 0,
+                AccreditationCode = "2020",
+                AccountantCode = "9090",
+                HolderId = 4,
+                DeclaredTypeId = 0,
+                DocumentId = 5050,
+                InvesmentConceptId = 2,
+                Module = false,
+                Frecuency = 1,
+                Isdac = 1,
+                Metrictons = "",
+                DeclarationDate = DateTime.Now
+            };
+            IInvesmentService invesmentService = new InvesmentService(_mockInvestmentRepository.Object, _mapper);
+            InvesmentDto invesmentDto = await invesmentService.EditAsync(id, invesmentSaveDto);
+
+
+            //Assert
+            Assert.Equal(invesment.Id, invesmentDto.Id);
+        }
+
+        [Fact]
+        public async void returnInvesmentDtoWhenDisableAsync()
+        {
+    // 
+            int id = 1;
+            //Arrange
+            Invesment invesment = new()
+            {
+                Id = 1,
+                Amount = 200,
+                Year = 2023,
+                Description = "description",
+                MiningConcessionId = 500,
+                Invesmenttypeid = 1,
+                CurrencyTypeId = 2,
+                PeriodTypeid = 0,
+                MeasureUnitId = 0,
+                RegistrationDate = DateTime.Now,
+                State = false,
+                MonthName = "--Seleccione--",
+                MonthId = 0,
+                AccreditationCode = "2020",
+                AccountantCode = "9090",
+                HolderId = 4,
+                DeclaredTypeId = 0,
+                DocumentId = 5050,
+                InvesmentConceptId = 2,
+                Module = false,
+                Frecuency = 1,
+                Isdac = 1,
+                Metrictons = "",
+                DeclarationDate = DateTime.Now
+            };
+
+            _mockInvestmentRepository
+             .Setup(r => r.FindByIdAsync(It.IsAny<int>()))
+             .ReturnsAsync(invesment);
+
+            _mockInvestmentRepository
+                .Setup(r => r.SaveAsync(It.IsAny<Invesment>()))
+                .ReturnsAsync(invesment);
+
+            //Act
+            IInvesmentService invesmentService = new InvesmentService(_mockInvestmentRepository.Object, _mapper);
+            InvesmentDto invesmentDto = await invesmentService.DisabledAsync(id);
+
+
+            //Assert
+            Assert.Equal(invesment.Id, invesmentDto.Id);
         }
     }
 
